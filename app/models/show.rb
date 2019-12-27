2577342a4e36d6207584d09a48e9503de2d033ad
github username <github email address>
2019-12-27 15:07:37 -0800

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
+  #A show belongs to a network and has many characters and has many actors through characters
+    belongs_to :network
+    has_many :characters
+    has_many :actors, through: :characters
+
+    def actors_list
+        self.actors.collect {|a| a.full_name }
+    end
 end
