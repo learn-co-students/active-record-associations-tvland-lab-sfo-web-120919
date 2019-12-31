d6d5973dd10b40b37743b214e417e40af9829771
github username <github email address>
2019-12-31 10:19:16 -0800

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,7 +1,7 @@
 class Character < ActiveRecord::Base
   #A character belongs to an actor and belongs to a show
-    belongs_to :actor 
-    belongs_to :show 
+    belongs_to :actor, :show 
+    # belongs_to :show 
 
     def say_that_thing_you_say
         "#{self.name} always says: #{self.catchphrase}"
